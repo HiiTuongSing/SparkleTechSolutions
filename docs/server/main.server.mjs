39a51a608046a80c10b0305e144f96997ea94cb3
HiiTuongSing <117565834+HiiTuongSing@users.noreply.github.com>
2024-05-07 14:08:07 +0800

    Added & Tested TailwindCSS

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-J7V6J763.mjs";import"./chunk-4VKIGONA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-OH5BMR2W.mjs";import"./chunk-F2NS5A5D.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
